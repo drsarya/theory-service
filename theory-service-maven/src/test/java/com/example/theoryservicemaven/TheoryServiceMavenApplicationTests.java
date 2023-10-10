14e7ee94b1ae37c631632374b76593402e44e97a
Stupak Darya <daria_stupak@relex.ru>
2023-10-10 18:00:48 +0300

    add proto

---
+++ theory-service-maven/src/test/java/com/example/theoryservicemaven/TheoryServiceMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theoryservicemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheoryServiceMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
